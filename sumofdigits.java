c5dbd2acfa7f6cd508d053f83d6a0cd08f12fef1
sandhiyak <sandhiyak@users.noreply.github.com>
2017-06-08 12:21:27 +0530

    Create sumofdigits.java

---
+++ sumofdigits.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class sumofdigits{
+         public static void main(String args[])
+         {
+         Scanner sc=new Scanner(System.in);
+         int n=sc.nextInt();
+         int sum=0;
+         while(n>0)
+         {
+         int num=n%10;
+         sum=sum+num;
+         n=n/10;
+         }
+         System.out.println("Sum of digits is"+sum);
+         }
+         }
