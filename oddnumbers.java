6b3016b96aa19702f00a2e63550064f72789c858
sandhiyak <sandhiyak@users.noreply.github.com>
2017-06-08 17:56:19 +0530

    Create oddnumbers.java

---
+++ oddnumbers.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class oddnumbers{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        for(int i=n;i<=m;i++)
+        {
+            if(i%2!=0)
+            System.out.println(i);
+        }
+        
+        
+    }
+}
