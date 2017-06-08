db46cdb560cd6032f69ffcf90cff8c90196c9d28
sandhiyak <sandhiyak@users.noreply.github.com>
2017-06-08 18:13:31 +0530

    Create evennumbers.java

--- evennumbers.java
+++ evennumbers.java
@@ -7,7 +7,7 @@ class oddnumbers{
         int m=sc.nextInt();
         for(int i=n;i<=m;i++)
         {
-            if(i%2!=0)
+            if(i%2==0)
             System.out.println(i);
         }
         }
