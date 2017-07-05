c5a99125230a81c64c2821429020474afab553cc
sandhiyak <sandhiyak@users.noreply.github.com>
2017-07-05 12:24:53 +0530

    Create stringtoint.java

---
+++ stringtoint.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class stringtoint
+{
+    public static void main(String args[])
+    {
+       
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        int a=Integer.parseInt(s);
+            System.out.print(a);
+    }
+}
